31abdfd7365d49e8b7c844908381e2f7bc4c4e59
Diogo <moozdzn@gmail.com>
2020-06-22 16:35:06 +0100

    Added timer to calculate user travelled distance every few minutes
    Refreshes tiles if user distances himself more than 2km CU-3wqb1a

--- AzulejosSrc/src/app/mapa/mapa.component.ts
+++ AzulejosSrc/src/app/mapa/mapa.component.ts
@@ -1,91 +1,65 @@
 // Angular Modules
 import { Component, ViewChild, OnInit, ViewContainerRef, ElementRef } from "@angular/core";
-import {registerElement} from "nativescript-angular/element-registry";
-import {ModalDialogService} from "nativescript-angular/directives/dialogs";
-import {RadAutoCompleteTextViewComponent} from "nativescript-ui-autocomplete/angular";
+import { registerElement } from "nativescript-angular/element-registry";
+import { ModalDialogService } from "nativescript-angular/directives/dialogs";
+import { RadAutoCompleteTextViewComponent } from "nativescript-ui-autocomplete/angular";
 // NativeScript Core Modules
-import {TokenModel} from "nativescript-ui-autocomplete";
-import {Accuracy} from "tns-core-modules/ui/enums";
-import {setInterval} from "tns-core-modules/timer";
+import { TokenModel } from "nativescript-ui-autocomplete";
+import { Accuracy } from "tns-core-modules/ui/enums";
+import { setInterval } from "tns-core-modules/timer";
 import { Page } from "tns-core-modules/ui/page"
 import { getNumber, setNumber, hasKey } from "tns-core-modules/application-settings";
 // External Packages
 import * as geolocation from "nativescript-geolocation";
 import * as Toast from 'nativescript-toast';
-import {isDarkModeEnabled} from "nativescript-dark-mode";
-import {localize} from "nativescript-localize";
-import {MapboxMarker, MapboxViewApi} from "nativescript-mapbox-enduco";
+import { isDarkModeEnabled } from "nativescript-dark-mode";
+import { localize } from "nativescript-localize";
+import { MapboxMarker, MapboxViewApi } from "nativescript-mapbox-enduco";
 // Azulejos Services
-import {UrlService} from "../shared/url.service";
-import {TileMarker} from "../shared/azulejos.models";
+import { UrlService } from "../shared/url.service";
+import { TileMarker } from "../shared/azulejos.models";
 // Azulejos Components
-import {TileDetailComponent} from "./tile-detail/tile-detail";
+import { TileDetailComponent } from "./tile-detail/tile-detail";
 
 registerElement("Mapbox", () => require("nativescript-mapbox-enduco").MapboxView);
 
-@Component({selector: "Mapa", templateUrl: "./mapa.component.html", styleUrls: ['./map.component.css']})
+@Component({ selector: "Mapa", templateUrl: "./mapa.component.html", styleUrls: ['./map.component.css'] })
 export class MapaComponent implements OnInit {
 
-    private isMap : boolean = true;
+    private isMap: boolean = true;
     private charCodes = [String.fromCharCode(0xf5a0), String.fromCharCode(0xf03a)];
-    
+
     private icon = this.charCodes[1];
-    private darkMode : string = "mapbox_streets";
+    private darkMode: string = "mapbox_streets";
     private userLocation;
-    private tiles : Array < TileMarker >;
-    private radius : number = 6;
-    private mapbox : MapboxViewApi;
-    private settingsDialog;
+    private tiles: Array<TileMarker>;
+    private radius: number = 6;
+    private mapbox: MapboxViewApi;
+    private visibility = "collapsed";
     private settingsBtn;
-    private checkUserPos;
-    
-    private isSettingsOpened : boolean = false;
 
+    private isSettingsOpened: boolean = false;
 
-    @ViewChild("autocomplete", {static: false}) private autocomplete : RadAutoCompleteTextViewComponent;
-    @ViewChild('settingDialog', {static: true}) private dialog : ElementRef;
-    @ViewChild('settingBtn', {static: true}) private dialogBtn : ElementRef;
+    @ViewChild("autocomplete", { static: false }) private autocomplete: RadAutoCompleteTextViewComponent;
+    @ViewChild('settingBtn', { static: true }) private dialogBtn: ElementRef;
 
-    constructor(private modal : ModalDialogService, 
-        private vcRef : ViewContainerRef, 
-        private _url : UrlService,
-        private _page: Page
-        ) {
-            this._page.actionBarHidden = true;
-        }
-
-    ngOnInit(): void {
-        if (isDarkModeEnabled()) {this.darkMode = "dark";}
-        if(hasKey('radius')) this.radius = getNumber('radius');
-    
-        this.settingsDialog = this.dialog.nativeElement;
-        this.settingsBtn = this.dialogBtn.nativeElement;
-
+    constructor(private modal: ModalDialogService,
+        private vcRef: ViewContainerRef,
+        private _url: UrlService,
+        private _page: Page) {
+        this._page.actionBarHidden = true;
     }
 
-    doAnimation(){
-        if(this.isSettingsOpened){
-            
-            this.settingsBtn.animate({
-                rotate: 0,
-                duration: 700
-            })
-            this.settingsDialog.visibility = "collapse";
-            this.isSettingsOpened = !this.isSettingsOpened
-        } else {
-            this.settingsDialog.visibility = "visible";
-            this.settingsBtn.animate({
-                rotate: 180,
-                duration: 700
-            })
-            this.isSettingsOpened = !this.isSettingsOpened
-        }
+    ngOnInit(): void {
+        if (isDarkModeEnabled()) { this.darkMode = "dark"; }
+        if (hasKey('radius')) this.radius = getNumber('radius');
+        this.settingsBtn = this.dialogBtn.nativeElement;
     }
 
     ngAfterViewInit(): void {
         this.autocomplete.autoCompleteTextView.loadSuggestionsAsync = () => {
             const promise = new Promise((resolve, reject) => {
-                this._url.requestTilesName().then((r : any) => {
+                this._url.requestTilesName().then((r: any) => {
                     const tilesCollection = r.docs;
                     const items: Array<TokenModel> = new Array();
                     for (let i = 0; i < tilesCollection.length; i++) {
@@ -102,8 +76,17 @@ export class MapaComponent implements OnInit {
             return promise;
         };
     }
-
-    
+    private doAnimation() {
+        if (this.isSettingsOpened) {
+            this.settingsBtn.animate({ rotate: 0, duration: 700 })
+            this.visibility = "collapse";
+            this.isSettingsOpened = !this.isSettingsOpened
+        } else {
+            this.visibility = "visible";
+            this.settingsBtn.animate({ rotate: 180, duration: 700 })
+            this.isSettingsOpened = !this.isSettingsOpened
+        }
+    }
 
     private onDidAutoComplete(args) {
         for (var i in this.tiles) {
@@ -113,12 +96,11 @@ export class MapaComponent implements OnInit {
             }
         }
     }
-    onMapReady(args) {
+    private onMapReady(args) {
         this.mapbox = args.map;
         geolocation.enableLocationRequest().then(() => {
-            geolocation.getCurrentLocation({desiredAccuracy: Accuracy.high}).then((location) => {
-                
-                this.mapbox.setCenter({lat: location.latitude, lng: location.longitude});
+            geolocation.getCurrentLocation({ desiredAccuracy: Accuracy.high }).then((location) => {
+                this.mapbox.setCenter({ lat: location.latitude, lng: location.longitude });
                 this.userLocation = {
                     lat: location.latitude,
                     lng: location.longitude
@@ -126,29 +108,10 @@ export class MapaComponent implements OnInit {
                 this.setTiles(this.userLocation);
             })
         })
-        this.checkUserPos = setInterval(() => {
-            geolocation.enableLocationRequest().then(() => {
-                geolocation.getCurrentLocation({desiredAccuracy: Accuracy.high}).then((location) => {
-                    var newLocation = {
-                        lat: location.latitude,
-                        lng: location.longitude
-                    }
-                    this.mapbox.getDistanceBetween(this.userLocation, newLocation).then((value : number) => {
-                        if (value > 2000) {
-                            this.userLocation = newLocation;
-                            this.setTiles(this.userLocation);
-                            console.log('Getting new markers');
-                        } else {
-                            console.log('New markers not needed');
-                        }
-                    })
-                })
-            })
-        }, 60000);
     }
 
     private setTiles(location) {
-        this._url.requestTilesNearUser(location, this.radius).then((r : any) => {
+        this._url.requestTilesNearUser(location, this.radius).then((r: any) => {
             var markers = [];
             this.tiles = [];
             for (var i in r) {
@@ -156,14 +119,14 @@ export class MapaComponent implements OnInit {
                     r[i].Localizacao.coordinates[1],
                     r[i].Localizacao.coordinates[0]
                 ], (r[i].distance / 1000).toFixed(2) + "km"))
-                markers.push(< MapboxMarker > {
+                markers.push(<MapboxMarker>{
                     id: this.tiles[i].id,
                     lat: this.tiles[i].coordinates[0],
                     lng: this.tiles[i].coordinates[1],
                     title: this.tiles[i].name,
                     subtitle: localize('discover.marker.subtitle'),
                     onTap: marker => {
-                        this.mapbox.setCenter({lat: marker.lat, lng: marker.lng})
+                        this.mapbox.setCenter({ lat: marker.lat, lng: marker.lng })
                     },
                     onCalloutTap: marker => {
                         this.openDetails(marker.id)
@@ -188,19 +151,18 @@ export class MapaComponent implements OnInit {
 
     private onSliderValueChange(args) {
         this.radius = args.value;
-        setNumber('radius',this.radius)
+        setNumber('radius', this.radius)
         this.setTiles(this.userLocation)
     }
 
     private centerUser(): void {
         geolocation.enableLocationRequest().then(() => {
-            geolocation.getCurrentLocation({desiredAccuracy: Accuracy.high}).then((location) => {
+            geolocation.getCurrentLocation({ desiredAccuracy: Accuracy.high }).then((location) => {
                 this.setTiles(this.userLocation);
-                this.mapbox.setCenter({lat: location.latitude, lng: location.longitude, animated: true})
-                var currentUserLocation = {
-                    lat: location.latitude,
-                    lng: location.longitude
-                }
+                this.mapbox.setCenter({ lat: location.latitude, lng: location.longitude, animated: false }).then(() => {
+                    this.mapbox.setZoomLevel({ level: 15, animated: false })
+                })
+                var currentUserLocation = { lat: location.latitude, lng: location.longitude }
                 this.userLocation = currentUserLocation;
             })
         })
@@ -219,11 +181,54 @@ export class MapaComponent implements OnInit {
             animated: true
         };
         this.modal.showModal(TileDetailComponent, options).then((cb) => {
-            if (cb == 0 || cb == null) 
+            if (cb == 0 || cb == null)
                 return
-             else 
+            else
                 this.openDetails(cb);
-            
+
         });
     }
+
+    private checkUserPos = setInterval(() => {
+        geolocation.enableLocationRequest().then(() => {
+            geolocation.getCurrentLocation({ desiredAccuracy: Accuracy.high }).then((location) => {
+                var newLocation = {
+                    lat: location.latitude,
+                    lng: location.longitude
+                }
+                if (this.distance(this.userLocation.lat, this.userLocation.lng, newLocation.lat, newLocation.lng) > 2) {
+                    this.userLocation = newLocation;
+                    this.setTiles(this.userLocation);
+                    console.log('Getting new markers');
+                } else {
+                    console.log('New markers not needed');
+                }
+            })
+        })
+    }, 30000);
+
+    
+
+    private distance(lat1, lon1, lat2, lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            var radlat1 = Math.PI * lat1 / 180;
+            var radlat2 = Math.PI * lat2 / 180;
+            var theta = lon1 - lon2;
+            var radtheta = Math.PI * theta / 180;
+            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+            if (dist > 1) {
+                dist = 1;
+            }
+            dist = Math.acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515;
+            dist = dist * 1.609344
+            console.log(dist)
+            return dist;
+        }
+    }
+
 }
